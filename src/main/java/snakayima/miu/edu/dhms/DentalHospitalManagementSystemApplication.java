371af4dd6c9904113c86133d0a23c929dbe53a15
Sandra Kawombe <sandrakawombe@gmail.com>
2024-12-10 21:50:21 -0600

    Initial commit: Set up project structure and entities

---
+++ src/main/java/snakayima/miu/edu/dhms/DentalHospitalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package snakayima.miu.edu.dhms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalHospitalManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentalHospitalManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/snakayima/miu/edu/dhms/DentalHospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package snakayima.miu.edu.dhms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalHospitalManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
